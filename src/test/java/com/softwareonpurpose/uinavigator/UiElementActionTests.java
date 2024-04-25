e34781ade9ebf4de7adaa7fb45a104bbdff969a6
Craig A. Stockton <cstockto@teksystems.com>
2024-04-25 10:35:45 -0700

    Added test locating Tag from ancestor located by ordinal, to ensure reliability of that scenario

--- src/test/java/com/softwareonpurpose/uinavigator/UiElementActionTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/UiElementActionTests.java
@@ -12,26 +12,33 @@ public class UiElementActionTests {
     @DataProvider
     public static Object[][] scenarios_click() {
         String attributeElementDescription = "'Width' attribute";
-        String tag = "img";
-        String attribute = "widt";
+        String tagImg = "img";
+        String attribute = "width";
         String attributeValue = "104";
+        String tagTr = "tr";
         final UiElement byTagPNotClickable =
                 UiElement.getInstance("tag 'p'", UiLocatorType.TAG, "p");
         final UiElement byTagA =
                 UiElement.getInstance("'Anchor' tag", UiLocatorType.TAG, "a");
         final UiElement byTagAOrdinal2 =
                 UiElement.getInstance("'Anchor' tag #2", UiLocatorType.TAG, "a", 2);
+        final UiElement byAttribute =
+                UiElement.getInstance(attributeElementDescription, UiLocatorType.TAG, tagImg, attribute, attributeValue);
+        final UiElement byOrdinal2 =
+                UiElement.getInstance("'TR' tag ordinal 2", UiLocatorType.TAG, tagTr, 2);
+        final UiElement byTagFromAncestorByOrdinal =
+                UiElement.getInstance("'TD' tag from 2nd 'TR' tag", UiLocatorType.TAG, "td", byOrdinal2);
         final String basicPage = "basic";
         final String linkPage = "link";
         final String imagePage = "image";
-        final UiElement byAttribute =
-                UiElement.getInstance(attributeElementDescription, UiLocatorType.TAG, tag, attribute, attributeValue);
+        final String tablePage = "tables";
         return new Object[][]{
                 {basicPage, byTagPNotClickable, "basic"}
                 , {basicPage, byTagA, "basic"}
                 , {linkPage, byTagA, "https://www.google.com/"}
                 , {linkPage, byTagAOrdinal2, "https://www.youtube.com/"}
                 , {imagePage, byAttribute, imagePage}
+                , {tablePage, byTagFromAncestorByOrdinal, tablePage}
         };
     }
 
