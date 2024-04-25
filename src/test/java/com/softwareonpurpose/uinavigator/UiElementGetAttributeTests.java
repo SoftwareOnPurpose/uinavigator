68825e7c0c771469e841daa1abc9201906d2408e
Craig A. Stockton <cstockto@teksystems.com>
2024-04-25 10:37:14 -0700

    Renamed static final members using ALL-CAPS to align with Java coding standards for constants

--- src/test/java/com/softwareonpurpose/uinavigator/UiElementGetAttributeTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/UiElementGetAttributeTests.java
@@ -7,41 +7,38 @@ import org.testng.annotations.Test;
 
 @Test
 public class UiElementGetAttributeTests {
-    private static final TestResource resources = TestResource.getInstance();
-    private static final UiElement byTagP =
+    private static final TestResource RESOURCE = TestResource.getInstance();
+    private static final UiElement BY_TAG_P =
             UiElement.getInstance("tag 'p'", UiLocatorType.TAG, "p");
-    private static final UiElement byTagA =
+    private static final UiElement BY_TAG_A =
             UiElement.getInstance("'Anchor' tag", UiLocatorType.TAG, "a");
-    private static final String stylePage = "style";
+    private static final UiElement BY_TAG_IMG =
+            UiElement.getInstance("'Image' element", UiLocatorType.TAG, "img");
+    private static final UiElement BY_TAG_NONEXISTENT =
+            UiElement.getInstance("tag nonexistent", UiLocatorType.TAG, "nonexistent");
+    private static final String STYLE_PAGE = "style";
+    private static final String IMAGE_PAGE = "image";
 
     @DataProvider
     public static Object[][] scenarios_getHref() {
         final String basicPage = "basic";
         final String linkPage = "link";
         return new Object[][]{
-                {linkPage, byTagP, null}
-                , {basicPage, byTagA, null}
-                , {linkPage, byTagA, "https://www.google.com/"}
+                {linkPage, BY_TAG_P, null}
+                , {basicPage, BY_TAG_A, null}
+                , {linkPage, BY_TAG_A, "https://www.google.com/"}
         };
     }
 
     @DataProvider
     public static Object[][] scenarios_getAttribute() {
-        final String imagePage = "image";
-        final UiElement byTagNonexistent =
-                UiElement.getInstance("tag nonexistent", UiLocatorType.TAG, "nonexistent");
-        final UiElement byTagImg =
-                UiElement.getInstance("'Image' element", UiLocatorType.TAG, "img");
-//        final UiElement byAttribute =
-//                UiElement.getInstance("'Attribute' element", UiLocatorType.TAG, "img", "width", "104");
         return new Object[][]{
-                {imagePage, byTagNonexistent, "src", null}
-                , {imagePage, byTagImg, "bogus", null}
-                , {imagePage, byTagImg, "src", "https://www.w3schools.com/html/w3schools.jpg"}
-                , {imagePage, byTagImg, "alt", "W3Schools.com"}
-                , {imagePage, byTagImg, "width", "104"}
-                , {stylePage, byTagP, "style", "color: red;"}
-//                , {imagePage, byAttribute, "height", "142"}
+                {IMAGE_PAGE, BY_TAG_NONEXISTENT, "src", null}
+                , {IMAGE_PAGE, BY_TAG_IMG, "bogus", null}
+                , {IMAGE_PAGE, BY_TAG_IMG, "src", "https://www.w3schools.com/html/w3schools.jpg"}
+                , {IMAGE_PAGE, BY_TAG_IMG, "alt", "W3Schools.com"}
+                , {IMAGE_PAGE, BY_TAG_IMG, "width", "104"}
+                , {STYLE_PAGE, BY_TAG_P, "style", "color: red;"}
         };
     }
 
@@ -51,9 +48,9 @@ public class UiElementGetAttributeTests {
         String bogus = "bogus";
         String red = "rgba(255, 0, 0, 1)";
         return new Object[][]{
-                {byTagA, color, null}
-                , {byTagP, bogus, ""}
-                , {byTagP, color, red}
+                {BY_TAG_A, color, null}
+                , {BY_TAG_P, bogus, ""}
+                , {BY_TAG_P, color, red}
         };
     }
 
@@ -64,21 +61,21 @@ public class UiElementGetAttributeTests {
 
     @Test(dataProvider = "scenarios_getHref")
     public void getHref(String page, UiElement element, String expected) {
-        UiHost.getInstance().load(resources.getPageUrl(page));
+        UiHost.getInstance().load(RESOURCE.getPageUrl(page));
         String actual = element.getHref();
         Assert.assertEquals(actual, expected);
     }
 
     @Test(dataProvider = "scenarios_getAttribute")
     public void getAttribute(String page, UiElement element, String attribute, String expected) {
-        UiHost.getInstance().load(resources.getPageUrl(page));
+        UiHost.getInstance().load(RESOURCE.getPageUrl(page));
         String actual = element.getAttribute(attribute);
         Assert.assertEquals(actual, expected);
     }
 
     @Test(dataProvider = "scenarios_getStyleProperty")
     public void getStyleProperty(UiElement element, String styleProperty, String expected) {
-        UiHost.getInstance().load(resources.getPageUrl(stylePage));
+        UiHost.getInstance().load(RESOURCE.getPageUrl(STYLE_PAGE));
         String actual = element.getStyleProperty(styleProperty);
         Assert.assertEquals(actual, expected);
     }
