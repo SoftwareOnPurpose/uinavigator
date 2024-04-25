f43d521162600092c7c304bbfb12b60313b67996
Craig A. Stockton <cstockto@teksystems.com>
2024-04-25 10:29:21 -0700

    Added 'ancestor' as a member of GetWebElementBehavior and made inclusion of an ancestor's CSS based on whether the ancstor is located by ordinal to provide reliable functionality within CSS limitations related to ordinals

--- src/main/java/com/softwareonpurpose/uinavigator/GetWebElementBehavior.java
+++ src/main/java/com/softwareonpurpose/uinavigator/GetWebElementBehavior.java
@@ -8,18 +8,18 @@ public abstract class GetWebElementBehavior {
     protected final By.ByCssSelector locator;
     protected final int ordinal;
     private final String css;
+    private final UiElement ancestor;
 
     protected GetWebElementBehavior(String locatorType, String locatorValue, String attribute, String attributeValue, Integer ordinal, UiElement ancestor) {
-        this.css = composeCss(locatorType, locatorValue, attribute, attributeValue, ancestor);
+        this.css = composeCss(locatorType, locatorValue, attribute, attributeValue);
         this.ordinal = ordinal == null || ordinal < 0 ? 0 : ordinal;
         this.locator = new By.ByCssSelector(this.css);
+        this.ancestor = ancestor;
     }
 
-    private static String composeCss(String locatorType, String locatorValue, String attribute, String attributeValue, UiElement ancestor) {
-        String ancestorCss = ancestor == null ? "" : ancestor.getCss();
+    private static String composeCss(String locatorType, String locatorValue, String attribute, String attributeValue) {
         String attributeCss = attribute == null ? "" : String.format("[%s='%s']", attribute, attributeValue);
-        //  img[width='104']
-        return String.format("%s %s", ancestorCss, String.format("%s%s%s", locatorType, locatorValue, attributeCss));
+        return String.format("%s%s%s", locatorType, locatorValue, attributeCss);
     }
 
     protected static String extractExceptionMessage(NoSuchElementException e) {
@@ -35,7 +35,7 @@ public abstract class GetWebElementBehavior {
     }
 
     String getCss() {
-        return css;
+        return ancestor.isByOrdinal() ? css : String.format("%s %s", ancestor.getCss(), css);
     }
 
     boolean isByOrdinal() {
